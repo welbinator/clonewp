a17d9ecd779b14e39f5c5dd12d21f0e990e05b4d
James <james.welbes@gmail.com>
2025-01-03 00:02:58 -0600

    update version number 1.1.0

--- clonewp.php
+++ clonewp.php
@@ -3,7 +3,7 @@
 Plugin Name: CloneWP
 Plugin URI: https://yourwebsite.com/plugin
 Description: A plugin to clone and display GitHub repositories.
-Version: 1.0.0
+Version: 1.1.0
 Author: Your Name
 Author URI: https://yourwebsite.com/
 License: GPL2
@@ -33,7 +33,7 @@ function wp_github_clone_enqueue_scripts($hook) {
         return;
     }
 
-    wp_enqueue_script('clonewp-script', plugin_dir_url(__FILE__) . 'admin/js/script.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('clonewp-script', plugin_dir_url(__FILE__) . 'admin/js/script.js', array('jquery'), '1.1.0', true);
     wp_localize_script('clonewp-script', 'wpGithubClone', array(
         'ajax_url' => admin_url('admin-ajax.php'),
         'nonce' => wp_create_nonce('wp_github_clone_nonce'),
@@ -49,7 +49,7 @@ function wp_github_clone_enqueue_admin_styles($hook) {
         return;
     }
 
-    wp_enqueue_style('clonewp-admin-style', plugin_dir_url(__FILE__) . 'admin/css/style.css', array(), '1.0.0');
+    wp_enqueue_style('clonewp-admin-style', plugin_dir_url(__FILE__) . 'admin/css/style.css', array(), '1.1.0');
 }
 add_action('admin_enqueue_scripts', 'wp_github_clone_enqueue_admin_styles');
 
