eacc54c71be7f26d0e00c11b62363e6af001641e
James <james.welbes@gmail.com>
2025-04-21 15:18:37 -0500

    update version numbe 1.1.1

--- clonewp.php
+++ clonewp.php
@@ -3,7 +3,7 @@
 Plugin Name: CloneWP
 Plugin URI: https://yourwebsite.com/plugin
 Description: A plugin to clone and display GitHub repositories.
-Version: 1.1.0
+Version: 1.1.1
 Author: Your Name
 Author URI: https://yourwebsite.com/
 License: GPL2
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define( 'CLONEWP_VERSION', '1.1.0' );
+define( 'CLONEWP_VERSION', '1.1.1' );
 define( 'CLONEWP_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CLONEWP_URL', plugin_dir_url( __FILE__ ) );
 define( 'CLONEWP_MIN_WP_VERSION', '5.8' );
@@ -44,7 +44,7 @@ function wp_github_clone_enqueue_scripts($hook) {
         return;
     }
 
-    wp_enqueue_script('clonewp-script', plugin_dir_url(__FILE__) . 'admin/js/script.js', array('jquery'), '1.1.0', true);
+    wp_enqueue_script('clonewp-script', plugin_dir_url(__FILE__) . 'admin/js/script.js', array('jquery'), CLONEWP_VERSION, true);
     wp_localize_script('clonewp-script', 'wpGithubClone', array(
         'ajax_url' => admin_url('admin-ajax.php'),
         'nonce' => wp_create_nonce('wp_github_clone_nonce'),
@@ -60,7 +60,7 @@ function wp_github_clone_enqueue_admin_styles($hook) {
         return;
     }
 
-    wp_enqueue_style('clonewp-admin-style', plugin_dir_url(__FILE__) . 'admin/css/style.css', array(), '1.1.0');
+    wp_enqueue_style('clonewp-admin-style', plugin_dir_url(__FILE__) . 'admin/css/style.css', array(), CLONEWP_VERSION);
 }
 add_action('admin_enqueue_scripts', 'wp_github_clone_enqueue_admin_styles');
 
